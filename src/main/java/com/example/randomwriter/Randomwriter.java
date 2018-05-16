5d691b564f7b63a568c9fa1f2a9ea796fc09a6f1
ssingularity <1755405704@qq.com>
2018-05-16 16:27:59 +0800

    add the actuator to monitor the application

--- src/main/java/com/example/randomwriter/Randomwriter.java
+++ src/main/java/com/example/randomwriter/Randomwriter.java
@@ -1,7 +1,14 @@
 package com.example.randomwriter;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import sun.misc.Cache;
+
+import javax.annotation.Resource;
+
 public class Randomwriter{
     private final long id;
+
     private final String content;
 
     public Randomwriter(long id, String content) {
